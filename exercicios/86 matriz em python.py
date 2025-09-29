ebc90ced10591773862a512cf154d4f3504a7d34
yuri <yuri.marinha@gmail.com>
2025-09-29 14:46:38 +0100

    exercice 85

--- exercicios/86 matriz em python.py
+++ exercicios/86 matriz em python.py
@@ -17,3 +17,13 @@ print(ma1)
 print(ma2)
 print(ma3)
 print('=-'*50)
+"""resolução do guanabara
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}], {c}]: '))
+print('-='*30)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]',end='')
+    print()"""
