69c72f9979af599969bad46c1869a7876919754f
yuri <yuri.marinha@gmail.com>
2025-09-30 12:55:27 +0100

    exercice 88

--- exercicios/87 Mais sobre Matriz em Python.py
+++ exercicios/87 Mais sobre Matriz em Python.py
@@ -32,3 +32,24 @@ print('=-'*50)
 print(f'a soma dos valores pares é {par}')
 print(f'o maior valor da segunda linha é {maior}')
 print(f'a soma dos valores da terceira coluna é {soma}')
+"""resolução do guanabara
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+spar = mai = scol = 0
+for l in range(0,3):
+    for C in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}: '))
+print('-='*30)
+for l in range(0,3):
+    for c in range(o,3):
+        print(f'[{matriz[l][c]:^5}], end='')
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print()
+print('-='*30)
+print(f'A soma dos valores pares é {spar}')
+for l in rage(0,3):
+    if c ==0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha é {mai}.')"""
--- exercicios/88  Palpites para a Mega Sena.py
+++ exercicios/88  Palpites para a Mega Sena.py
@@ -6,3 +6,28 @@ for c in range(0,vezes):
     listatotal.append(n)
 for p in listatotal:
     print(p)
+"""resolução do guanabara
+from random import radint
+from time import sleep
+lista = list()
+jogos = list()
+print('-'*30)
+print('      JOGA NA MEGAS SENA     ')
+print('-'*30)
+cont = 0
+tot = 0
+while True:
+    num = radint(1,60)
+        lista.append(num)
+        cont+=1
+    if cont >= 6:
+        break
+lista.sort()
+jogos.append(lista[:])
+lista.clear
+tot +=1
+print('-='*3, f'  SORTEADO {quant}  JOGOS', '-='*3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+print('-=' *5 , '< BOA SORTE! >', '-='*5)"""
