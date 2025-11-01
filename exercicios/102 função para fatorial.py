50135cc48a6860ca6089b04bcf3ab9f04a39e63b
yuri <yuri.marinha@gmail.com>
2025-11-01 09:07:12 +0000

    exercice 102 concluded

--- exercicios/102 função para fatorial.py
+++ exercicios/102 função para fatorial.py
@@ -1,9 +1,31 @@
-def função(num):
+def função(num,show=False):
+    """fatorial(n,show=false)
+        -> Calcula o fatorial de um número.
+         :param n: O número a ser calculado
+         :param show: (opcional) Mostrar ou não a conta
+         :return: O valor do fatorial de um número n"""
     total = 1
     for c in range (num,0,-1):
         total = total * c
-        print(f'{c} x',end=' ')
-    print('=',total)
+        if show == True:
+            if c > 1:
+                print(f'{c} ',end='x ')
+            else:
+                print(f'{c} = ', end='')
+    print(total)
 
-
-função(10)
+função(5,True)
+"""resolução do guanabara
+def fatorial(n,show=False):
+    f = 1
+    for c in range(n, 0, -1):
+        print(c,end='')
+        if c > 1:
+            print(' x ',end='')
+        else:
+            print(' = ',end='')
+        f *= c
+    return f
+    
+#programa principal
+print(fatorial(5,show=True))"""
