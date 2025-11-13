c1c6f147153c83af424465a4413d8f9fd3d985ae
yuri <yuri.marinha@gmail.com>
2025-11-13 23:58:16 +0000

    exercice 107 concluded

---
+++ exercicios/aula 107.py
@@ -0,0 +1,6 @@
+from modulos import mod107
+
+n=int(input('Digite um número: '))
+mod107.metade(n)
+mod107.dobro(n)
+mod107.porcento(n)
---
+++ exercicios/modulos/mod107.py
@@ -0,0 +1,8 @@
+def metade(n):
+    print(f'A metade de {n} é de {n/2}')
+
+def dobro(n):
+    print(f'O dobro de {n} é de {n*2}')
+
+def porcento(n):
+    print(f'Aumentando em 10% o valor {n} fica em torno de {n+((n*10)/100)}')
